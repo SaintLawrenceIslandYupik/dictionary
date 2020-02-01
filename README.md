01fb1fadc64aeeeca6abf2f67276ce8e51cdf43d
Lane Schwartz <dowobeha@gmail.com>
2020-01-31 19:35:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# St. Lawrence Island Dictionary
+
+This repository hosts files and conversion scripts related to the <i>St. Lawrence Island / Siberian Yupik Eskimo Dictionary</i> compiled by Linda Womkon Badten (Aghnaghaghpik), Vera Oovi Keneshiro (Uqiitlek), Marie Oovi (Uvegtu), and Christopher Koonooka (Petuwaq); edited by Steven A. Jacobson; and published by the Alaska Native Language Center at the University of Alaska Fairbanks in 2008.
+
+## Source files
+
+The [data](data) directory contains the following files that were extracted from the original **FileMaker Pro** source files:
+* [data/bases.rtf](bases.rtf)
+* [data/bases.html](bases.html)
+* [data/postbases.rtf](postbases.rtf)
+* [data/postbases.html](postbases.html)
+
+These files should not be edited.
+
+
+### RTF generation procedure
+
+The RTF files were produced using the following procedure:
+1. The relevant **Filemaker Pro** file was opened.
+2. Within **Filemaker Pro**, the *Script Workspace* was opened, and a new script was creating. The scripting command *Copy All Records/Requests* was added to the file, and the script was saved and then run.
+3. At this point, the macOS clipboard contained the contents of the **Filemaker Pro** file in rich text format (RTF).
+4. The macOS application **TextEdit** was opened, and a new document was created. The contents of the clipboard were pasted into the new document, which was then saved.
+
+Each line in the resulting RTF file contains one dictionary entry from the **Filemaker Pro** file. The fields within each line are delineated with a tab character.
+
+
+### HTML generation procedure
+
+The HTML files were produced using the following procedure:
+1. The relevant **Filemaker Pro** file was opened.
+2. Within **Filemaker Pro**, the *Script Workspace* was opened, and a new script was creating. The scripting command *Copy All Records/Requests* was added to the file, and the script was saved and then run.
+3. At this point, the macOS clipboard contained the contents of the **Filemaker Pro** file in rich text format (RTF).
+4. The macOS application **Terminal** was opened, and the command `pbpaste -Prefer rtf | textutil -stdin -convert html -output foo.html` was run, where `base` or `postbase` was substituted for `foo` as appropriate.
+
+
+## Code
+
+The [code](code) directory contains the following files:
+* [code/html2xml.py](html2xml.py)
+
+This Python script requires the [https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup](Beautiful Soup 4) Python library, which can be installed in a local virtual environment as follows:
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip3 install beautifulsoup4
+deactivate
+```
+
+The code can then be run as follows:
+
+```
+source venv/bin/activate
+./code/html2xml.py data/foo.html
+deactivate
+```
+
+Where `base` or `postbase` is substituted for `foo` as appropriate.
+
+This code is responsible for cleaning up the formatting tags and whitespace in the original html file, and creating the XML files. The idea is that this script should contain all of the tweaks and hacks necessary to handle any oddities in the original source files. The original source files themselves should not be edited.
+
+
+## License
+
+**This resource is part of the linguistic and cultural heritage of the St. Lawrence Island Yupik people. By accessing this resource, you agree to treat this resource, the St. Lawrence Island Yupik language, the St. Lawrence Island Yupik culture, and the St. Lawrence Island Yupik people with dignity and respect. If you do not agree to these conditions, you may not access this resource and you may not make copies of this resource.**
+
+If you agree to these conditions, you may access this resource under the terms of the Creative Commons Attribution No-Commercial 4.0 license (https://creativecommons.org/licenses/by-nc/4.0/).
---
+++ code/html2xml.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+from typing import List
+import re
+import sys
+from bs4 import BeautifulSoup
+
+class Entry:
+
+    field_names = [
+        'CSY word (Lat. orth.)',
+        'CSY word (Cyr. orth.)',
+        'Eng. gloss 1',
+        'Russ. gloss 1',
+        "ex'ples, etym, etc.",
+        'Eng. gloss 2',
+        'Eng. gloss 3',
+        'Russ. gloss 2',
+        'Russ. gloss 3',
+        'CSY word (Cyr.) num. eq.',
+        'Russ. gloss 1 num. eq.',
+        'Russ. gloss 2 num. eq.',
+        'Russ. gloss 3 num. eq.',
+        'queries, etc.',
+        'Eng. gloss 4',
+        'Russ. gloss 4',
+        'Russ. gloss 4 num. eq.',
+        'combined English gloss',
+        'combined Russian gloss',
+        'PE (PY) source',
+        'symantic code',
+        'Sib. source code',
+        'derived forms',
+        'postbase head form',             # Only occurs in postbases
+        'postbase alphabetization form',  # Only occurs in postbases
+        'alphabetization field A',        # Only occurs in     bases
+        'alphabetization field B',
+        'CSY word (Cyrillic)'
+    ]
+
+
+    def __init__(self, *, fields: List[str]):
+        if len(fields) != 28:
+            raise ValueError("There must be exactly 28 fields, but there were {len(fields)}")
+        self._fields = dict(zip(Entry.field_names, fields))
+        
+        self.latin = self.extract(0)
+        self.cyrillic = self.extract(-1)
+        self.coded_cyrillic = self.extract(1)
+        
+        self.examples = self.extract(4)
+        
+        self.combined_english_gloss = self.extract(17)
+
+    def extract(self, index: int) -> str:
+        result = self._fields[Entry.field_names[index]]
+        
+        result = result.replace('\xa0','')
+        
+        result = re.sub(r'<span class="Apple-converted-space">\s*</span>', ' ', result)
+        
+        result = result.replace('yu<u>k', 'yu(u)k')
+        
+        if result.startswith('</i>') or result.startswith('</b>'):
+            result = result[4:]
+        elif result.startswith('</sup>') or result.startswith('</sub>'):
+            result = result[6:]
+        
+        open_i = result.count('<i>')
+        close_i = result.count('</i>')
+        
+        open_b = result.count('<b>')
+        close_b = result.count('</b>')
+
+        open_sup = result.count('<sup>')
+        close_sup = result.count('</sup>')
+        
+        open_sub = result.count('<sub>')
+        close_sub = result.count('</sub>')
+
+        for tag in ['sup', 'sub', 'i', 'b']:
+            opening_tag = f"<{tag}>"
+            closing_tag = f"</{tag}>"
+            
+            if result.startswith(closing_tag):
+                result = result[len(closing_tag):]  # String shouldn't start with a closing tag
+        
+            count_open = result.count(opening_tag)
+            count_close = result.count(closing_tag)
+            
+            if count_open > count_close:
+                #print(f"Too many {opening_tag}:\t{count_open} > {count_close}\t{result}")
+                result = result + closing_tag
+            
+            if count_open < count_close:
+                #print(f"Too many {closing_tag}:\t{count_open} < {count_close}\t{result}")
+                result = opening_tag + result
+            
+            if result.find(closing_tag) < result.find(opening_tag):
+                result = opening_tag + result
+                
+            if result.rfind(opening_tag) > result.find(closing_tag):
+                result = result + closing_tag
+
+            while f" {closing_tag}" in result or f";{closing_tag}" in result:
+                result = result.replace(f" {closing_tag}", f"{closing_tag} ")
+                result = result.replace(f";{closing_tag}", f"{closing_tag};")
+
+        return result
+
+    def __str__(self):
+        return f"""\
+    <entry>
+        <orthography type="latin">{self.latin}</orthography>
+        <orthography type="cyrillic" code="{self.coded_cyrillic}">{self.cyrillic}</orthography>
+    
+        <glosses lang="eng">{self.combined_english_gloss}</glosses>
+    
+        <examples>{self.examples}</examples>
+    </entry>
+"""
+
+def load(*, filename: str) -> BeautifulSoup:
+    with open(filename) as html_file:
+        print(f"Loading file {filename} ... ", end="", flush=True, file=sys.stderr)
+        html = BeautifulSoup(html_file, "html.parser")
+        print("done.", end="\n", flush=True, file=sys.stderr)
+        return html
+
+def parse(*, filename: str):
+    html = load(filename=sys.argv[1])
+
+    entries = html.body.find_all("p")
+    print(len(entries))
+    
+    pattern = re.compile(r'<span class="Apple-tab-span">\s*</span>')
+    
+    for num, entry in enumerate(entries):
+        body = ''.join([str(item) for item in entry.contents])
+        parts = pattern.split(body)
+        if len(parts) == 26:  # base
+            parts.insert(-3, '') # Insert empty string for 'postbase head form'
+            parts.insert(-3, '') # Insert empty string for 'postbase alphabetization form'
+        elif len(parts) == 27:  # post-base
+            parts.insert(-2, '') # Insert empty string for 'alphabetization field A'
+        
+        entry = Entry(fields=parts)
+        print(entry)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print(f"Usage:\t{sys.argv[0]} lexicon.html", file=sys.stderr, flush=True)
+        sys.exit(0)
+    
+    else:
+        parse(filename=sys.argv[1])
