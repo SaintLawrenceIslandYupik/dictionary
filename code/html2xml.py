25319d9b4da5fff3b575a7c60001e041b24b159f
Lane Schwartz <dowobeha@gmail.com>
2020-02-28 13:39:05 -0600

    Refactored and added first unit test

--- code/html2xml.py
+++ code/html2xml.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-from typing import List
+import logging
+from typing import Dict, List
 import re
 import sys
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 
 
 class Entry:
@@ -39,18 +41,30 @@ class Entry:
         'CSY word (Cyrillic)'
     ]
 
-    def __init__(self, *, fields: List[str]):
-        if len(fields) != 28:
+    def __init__(self, html_entry: "HtmlEntry"):
+        if len(html_entry) != 28:
             raise ValueError("There must be exactly 28 fields, but there were {len(fields)}")
-        self._fields = dict(zip(Entry.field_names, fields))
+        self._fields = dict(zip(Entry.field_names, list(html_entry)))
         
         self.latin = self.extract(0)
         self.cyrillic = self.extract(-1)
         self.coded_cyrillic = self.extract(1)
         
-        self.examples = self.extract(4)
+        self.raw_examples = self.extract(4)
+        self.examples = self.raw_examples.split("; ")
+
+        if len(self.examples) > 0 and self.examples[0] == '<i>particle</i>':
+            self.examples = self.examples[1:]
+            self.part_of_speech = "particle"
+        elif len(self.examples) > 0 and self.examples[0] == '<i>adverbial particle</i>':
+            self.examples = self.examples[1:]
+            self.part_of_speech = "adverbial particle"
+        else:
+            self.part_of_speech = None
+
+        self.examples = [Example(example) for example in self.examples]
         
-        self.combined_english_gloss = self.extract(17)
+        self.combined_english_gloss = self.extract(17).split("; ")
 
     def extract(self, index: int) -> str:
         result = self._fields[Entry.field_names[index]]
@@ -65,18 +79,6 @@ class Entry:
             result = result[4:]
         elif result.startswith('</sup>') or result.startswith('</sub>'):
             result = result[6:]
-        
-        open_i = result.count('<i>')
-        close_i = result.count('</i>')
-        
-        open_b = result.count('<b>')
-        close_b = result.count('</b>')
-
-        open_sup = result.count('<sup>')
-        close_sup = result.count('</sup>')
-        
-        open_sub = result.count('<sub>')
-        close_sub = result.count('</sub>')
 
         for tag in ['sup', 'sub', 'i', 'b']:
             opening_tag = f"<{tag}>"
@@ -89,17 +91,19 @@ class Entry:
             count_close = result.count(closing_tag)
             
             if count_open > count_close:
-                # print(f"Too many {opening_tag}:\t{count_open} > {count_close}\t{result}")
+                # There shouldn't be too many opening tags
                 result = result + closing_tag
             
             if count_open < count_close:
-                # print(f"Too many {closing_tag}:\t{count_open} < {count_close}\t{result}")
+                # There shouldn't be too many closing tags
                 result = opening_tag + result
             
             if result.find(closing_tag) < result.find(opening_tag):
+                # The first closing tag shouldn't be before the first opening tag
                 result = opening_tag + result
                 
-            if result.rfind(opening_tag) > result.find(closing_tag):
+            if result.rfind(opening_tag) > result.rfind(closing_tag):
+                # The last opening tag shouldn't be after the last closing tag
                 result = result + closing_tag
 
             while f" {closing_tag}" in result or f";{closing_tag}" in result:
@@ -109,45 +113,113 @@ class Entry:
         return result
 
     def __str__(self):
+        newline = "\n"
         return f"""\
-    <entry>
-        <orthography type="latin">{self.latin}</orthography>
-        <orthography type="cyrillic" code="{self.coded_cyrillic}">{self.cyrillic}</orthography>
+    <entry part-of-speech="{self.part_of_speech}">
     
-        <glosses lang="eng">{self.combined_english_gloss}</glosses>
+        <orthographic-form type="latin">{self.latin}</orthographic-form>
+        <orthographic-form type="cyrillic" code="{self.coded_cyrillic}">{self.cyrillic}</orthographic-form>
     
-        <examples>{self.examples}</examples>
+        <glosses>
+{newline.join(['            <gloss lang="eng">' + gloss + '</gloss>' for gloss in self.combined_english_gloss])}
+        </glosses>
+
+        <examples>
+
+{newline.join([str(e) for e in self.examples])}
+        </examples>
+        
     </entry>
 """
 
 
-def load(*, filename: str) -> BeautifulSoup:
-    with open(filename) as html_file:
-        print(f"Loading file {filename} ... ", end="", flush=True, file=sys.stderr)
-        html = BeautifulSoup(html_file, "html.parser")
-        print("done.", end="\n", flush=True, file=sys.stderr)
-        return html
+class Example:
+
+    def __init__(self, example_string: str):
+        yupik_start = 0
+        yupik_end = example_string.find('‘') - 1 if '‘' in example_string else len(example_string)
+        english_start = yupik_end + 1 if '‘' in example_string else len(example_string)
+        english_end = example_string.find('’')+1 if '’' in example_string else len(example_string)
+        citation_start = english_end if '(' in example_string[english_end:] else len(example_string)
+        citation_end = len(example_string)
+        self.yupik = example_string[yupik_start:yupik_end].strip()
+        self.english = example_string[english_start:english_end].strip()
+        self.citation = example_string[citation_start:citation_end].strip()
+
+    def __str__(self):
+        if len(self.yupik) == 0 and len(self.english) == 0 and len(self.citation) == 0:
+            return ""
+
+        result = "            <example>\n"
+        if len(self.yupik) > 0:
+            result += "                <yupik-example>" + self.yupik + "</yupik-example>\n"
+        if len(self.english) > 0:
+            result += "                <english-example>" + self.english + "</english-example>\n"
+        if len(self.citation) > 0:
+            result += "                <citation>" + self.citation + "</citation>\n"
+        result += "            </example>\n"
+        return result
 
 
-def parse(*, filename: str):
-    html = load(filename=sys.argv[1])
+class HtmlEntry:
 
-    entries = html.body.find_all("p")
-    print(f"Identified {len(entries)} entries in {filename}.", flush=True, file=sys.stderr)
-    
-    pattern = re.compile(r'<span class="Apple-tab-span">\s*</span>')
-    
-    for num, entry in enumerate(entries):
+    def __init__(self, fields: List[str]):
+        if len(fields) != 28:
+            raise ValueError(f"There must be exactly 28 fields, but there were {len(fields)}")
+        self._fields: List[str] = fields
+
+    def __getitem__(self, item):
+        return self._fields[item]
+
+    def __len__(self):
+        return len(self._fields)
+
+    def __iter__(self):
+        return iter(self._fields)
+
+    def __str__(self):
+        return "\n".join(self._fields)
+
+
+class HtmlDictionary:
+
+    delimiter = re.compile(r'<span class="Apple-tab-span">\s*</span>')
+
+    def __init__(self, filename: str):
+        with open(filename) as html_file:
+            html = BeautifulSoup(html_file, "html.parser")
+            self._tags: List[Tag] = html.body.find_all("p")
+            self._entries = [None] * len(self._tags)
+
+    def __getitem__(self, index) -> List[HtmlEntry]:
+        if isinstance(index, int):
+            if self._entries[index] is None:
+                self._entries[index] = HtmlDictionary.parse_entry(self._tags[index])
+            return self._entries[index]
+        else:
+            raise ValueError
+
+    def __len__(self) -> int:
+        return len(self._entries)
+
+    @staticmethod
+    def parse_entry(entry: Tag) -> HtmlEntry:
         body = ''.join([str(item) for item in entry.contents])
-        parts = pattern.split(body)
+        parts = HtmlDictionary.delimiter.split(body)
         if len(parts) == 26:  # base
             parts.insert(-3, '')  # Insert empty string for 'postbase head form'
             parts.insert(-3, '')  # Insert empty string for 'postbase alphabetization form'
         elif len(parts) == 27:  # post-base
             parts.insert(-2, '')  # Insert empty string for 'alphabetization field A'
-        
-        entry = Entry(fields=parts)
-        print(entry)
+        return HtmlEntry(parts)
+
+    @staticmethod
+    def load(*, filename: str) -> BeautifulSoup:
+        with open(filename) as html_file:
+            logging.info(f"Loading file {filename} using BeautifulSoup")
+            html = BeautifulSoup(html_file, "html.parser")
+            logging.info(f"File {filename} loaded")
+            return html
 
 
 if __name__ == "__main__":
@@ -157,4 +229,5 @@ if __name__ == "__main__":
         sys.exit(0)
     
     else:
-        parse(filename=sys.argv[1])
+        for entry in HtmlDictionary(filename=sys.argv[1]):
+            print(Entry(entry))
---
+++ test/katamqun.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from html2xml import HtmlDictionary, HtmlEntry, Entry
+
+
+class TestKatamqunHtmlEntry(unittest.TestCase):
+
+    def setUp(self):
+        self.entry: HtmlEntry = HtmlDictionary("katamqun.html")[0]
+
+    def test_length(self):
+        self.assertEqual(len(self.entry), 28)
+
+    def test_latin(self):
+        self.assertEqual(self.entry[0], "katamqun")
+
+    def test_cyrillic_code(self):
+        self.assertEqual(self.entry[1], "rfnfv2ey")
+
+    def test_english_gloss1(self):
+        self.assertEqual(self.entry[2], "barely")
+
+    def test_russian_gloss1(self):
+        self.assertEqual(self.entry[3], "")
+
+    def test_examples(self):
+        self.assertEqual(self.entry[4], """<i>particle;<span class="Apple-converted-space">  </span></i>katamqun taawa qayuusim imaghhaa siipsimalghii pagunghaq saghnakaq ‘there are only a cup left of the berries that were picked’; Maaten ilangani nunalgutangit qamaglluteng Quyillegqaasimalghiit. <i>Katamqun</i> yaywaalingiighhaak<span class="Apple-converted-space">  </span>unegsimalghiik. ‘Then one time their fellow villagers all went off with the Chukchis. <i>Only</i> the two little orpans remained behind.’ (UNGAZ. UNGIP. 61)""")
+
+    def test_english_gloss2(self):
+        self.assertEqual(self.entry[5], "only")
+
+    def test_english_gloss3(self):
+        self.assertEqual(self.entry[6], "")
+
+    def test_russian_gloss2(self):
+        self.assertEqual(self.entry[7], "")
+
+    def test_russian_gloss3(self):
+        self.assertEqual(self.entry[8], "")
+
+    def test_csy_word_cyr_num_eq(self):
+        self.assertEqual(self.entry[9], "")
+
+    def test_russian_gloss_1_num_eq(self):
+        self.assertEqual(self.entry[10], "")
+
+    def test_russian_gloss_2_num_eq(self):
+        self.assertEqual(self.entry[11], "")
+
+    def test_russian_gloss_3_num_eq(self):
+        self.assertEqual(self.entry[12], "")
+
+    def test_queries_etc(self):
+        self.assertEqual(self.entry[13], "")
+
+    def test_english_gloss4(self):
+        self.assertEqual(self.entry[14], "")
+
+    def test_russian_gloss4(self):
+        self.assertEqual(self.entry[15], "")
+
+    def test_russian_gloss3_num_eq(self):
+        self.assertEqual(self.entry[16], "")
+
+    def test_english_gloss_combined(self):
+        self.assertEqual(self.entry[17], "barely; only")
+
+    def test_russian_gloss_combined(self):
+        self.assertEqual(self.entry[18], "")
+
+    def test_pe_py_source(self):
+        self.assertEqual(self.entry[19], "< katam=qun")
+
+    def test_symantic_code(self):
+        self.assertEqual(self.entry[20], "")
+
+    def test_sib_source_code(self):
+        self.assertEqual(self.entry[21], "")
+
+    def test_derived_forms(self):
+        self.assertEqual(self.entry[22], "")
+
+    def test_postbase_head_form(self):
+        self.assertEqual(self.entry[23], "")
+
+    def test_postbase_alphabetization_form(self):
+        self.assertEqual(self.entry[24], "")
+
+    def test_alphabetization_field_a(self):
+        self.assertEqual(self.entry[25], "katamqun")
+
+    def test_alphabetization_field_a(self):
+        self.assertEqual(self.entry[26], "katamqun")
+
+    def test_cyrillic(self):
+        self.assertEqual(self.entry[27], "катамқун")
+
+
+class TestKatamqunEntry(unittest.TestCase):
+
+    def setUp(self):
+        self.entry: Entry = Entry(HtmlDictionary("katamqun.html")[0])
+
+    def test_latin(self):
+        self.assertEqual(self.entry.latin, "katamqun")
+
+    def test_cyrillic(self):
+        self.assertEqual(self.entry.cyrillic, "катамқун")
+
+    def test_cyrillic_code(self):
+        self.assertEqual(self.entry.coded_cyrillic, "rfnfv2ey")
+
+    def test_raw_examples(self):
+        self.assertEqual(self.entry.raw_examples, """<i>particle</i>; katamqun taawa qayuusim imaghhaa siipsimalghii pagunghaq saghnakaq ‘there are only a cup left of the berries that were picked’; Maaten ilangani nunalgutangit qamaglluteng Quyillegqaasimalghiit. <i>Katamqun</i> yaywaalingiighhaak unegsimalghiik. ‘Then one time their fellow villagers all went off with the Chukchis. <i>Only</i> the two little orpans remained behind.’ (UNGAZ. UNGIP. 61)""")
+
+    def test_examples(self):
+        self.assertEqual(len(self.entry.examples), 2)
+        self.assertEqual(self.entry.examples[0].yupik, """katamqun taawa qayuusim imaghhaa siipsimalghii pagunghaq saghnakaq""")
+        self.assertEqual(self.entry.examples[0].english, """‘there are only a cup left of the berries that were picked’""")
+        self.assertEqual(self.entry.examples[0].citation, "")
+        self.assertEqual(self.entry.examples[1].yupik, """Maaten ilangani nunalgutangit qamaglluteng Quyillegqaasimalghiit. <i>Katamqun</i> yaywaalingiighhaak unegsimalghiik.""")
+        self.assertEqual(self.entry.examples[1].english, """‘Then one time their fellow villagers all went off with the Chukchis. <i>Only</i> the two little orpans remained behind.’""")
+        self.assertEqual(self.entry.examples[1].citation, """(UNGAZ. UNGIP. 61)""")
+
+    def test_combined_english_gloss(self):
+        self.assertEqual(len(self.entry.combined_english_gloss), 2)
+        self.assertEqual(self.entry.combined_english_gloss[0], "barely")
+        self.assertEqual(self.entry.combined_english_gloss[1], "only")
+
+    def test_part_of_speech(self):
+        self.assertEqual(self.entry.part_of_speech, """particle""")
+
+
+if __name__ == '__main__':
+    unittest.main()
