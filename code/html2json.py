2439813e79206e6a8ae2e079675145335f0cee55
bhunt6 <31418577+bhunt6@users.noreply.github.com>
2022-11-30 11:59:26 -0500

    add rootGen method; catch lines with only spaces

--- code/html2json.py
+++ code/html2json.py
@@ -14,10 +14,15 @@ class JsonEntry:
         self.UUID = hashlib.sha1(bytes(self.entry.temp, 'utf-8'))
         search_word = re.sub(r'\<\/*\w+\>', '', self.entry.latin)
         self.entry.search = re.sub(r'[^a-zA-Z]+', '', search_word)
-        self.entry.ipa = self.phoneticize(self.entry.search)
+        self.entry.root = self.rootGen(self.entry.search)
+        self.entry.ipa = self.phoneticize(self.entry.root)
 
 
     def __str__(self):
+        for i, element in enumerate(self.entry.combined_english_gloss):
+            self.entry.combined_english_gloss[i] = element.replace('"', "&#34;")
+            self.entry.combined_english_gloss[i] = element.replace('"', "&#34;")
+
         gloss_string = '[' + ", ".join([f'"{gloss}"' for gloss in self.entry.combined_english_gloss]) + '],'
         note_string = '[' + ", ".join([f'"{gloss}"' for gloss in self.entry.notes]) +  '],'
         example_string = '[' + ", ".join([f'"{gloss}"' for gloss in self.entry.examples]) + '],'
@@ -26,9 +31,10 @@ class JsonEntry:
         example_string = self.listFormat(example_string)
         
         return f"""
-{{"UUID":"{self.UUID.hexdigest()}",
+"{self.UUID.hexdigest()}":{{
 "search_word":"{self.entry.search}",
 "headword":"{self.entry.latin}",
+"root":"{self.entry.root}",
 "cyrillic":"{self.entry.cyrillic}",
 "ipa":"{self.entry.ipa}",
 "jacobson":"{self.entry.coded_cyrillic}",
@@ -37,7 +43,7 @@ class JsonEntry:
 "notes":{note_string}
 "examples":{example_string}
 "source":"{self.entry.source if self.entry.source != '' else "Badten et al, 2001"}",
-"etymology":"{self.entry.etymology.replace('<', '&lt;')}",
+"etymology":"{self.entry.etymology.replace('< ', '&lt; ')}",
 "semantic_code":"{self.entry.semantic_code}",
 "alphaA":"{self.entry.alphabetizationA}",
 "alphaB":"{self.entry.alphabetizationB}"
@@ -62,6 +68,7 @@ class JsonEntry:
         result = result.replace("</english-example>\n", "</span>")
         result = result.replace("<citation>", "\n\t\t<span class='citation'>")
         result = re.sub(r"</citation>\n\s*", "</span>", result)
+        result = re.sub(r"\n\s*", "", result)
        #result = result.replace("</citation>", "</span>")
         return result
 
@@ -70,6 +77,38 @@ class JsonEntry:
         result = redouble(result)
         result = convert2ipa(result)
         result = tokens2string(result)
+        return result
+
+    def rootGen(self, word):
+        #verbs
+        if "-" in self.entry.latin:
+            result = word
+        #nouns: "kw"
+        elif word[-2:] == "kw":
+            result = word[:-2] + "w"
+        #nouns: "qw"
+        elif word[-2:] == "qw":
+            result = word[:-2] + "ghw"
+        #nouns: marked strong gh
+        elif "*" in self.entry.latin:
+            result = word[:-1] + "gh*"
+        #nouns: all other gh - "qikmiq"
+        elif word[-1] == "q":
+            result = word[:-1] + "gh"
+        #nouns: g - "sikik"
+        elif word[-1] == "k":
+            result = word[:-1] + "g"
+        #noun:e-underlying - "aaggaatae" 
+        elif word[-2:] == "ae":
+            result = word[:-2] + "e"
+        #vowel final roots: "repa"(N), "aafte"(V)
+        elif word[-1] in ["a", "i", "u", "e"]:
+            result = word
+        #nouns: te
+        elif word[-1] == "n":
+            result = word[:-1] + "te"
+        else:
+            result = word
         return result
 
 if __name__ == "__main__":
