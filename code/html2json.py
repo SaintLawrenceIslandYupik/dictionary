072ed934c501205bbf8c364bb6fba717a8ee05db
BHunt <benjaminhunt93@gmail.com>
2024-04-24 23:43:38 -0500

    separate entries with multiple headwords

--- code/html2json.py
+++ code/html2json.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import logging, sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 import re
 import hashlib
@@ -12,11 +14,12 @@ class JsonEntry:
         self.entry = entry
         self.entry.temp = self.entry.latin + self.entry.combined_english_gloss[0]
         self.UUID = hashlib.sha1(bytes(self.entry.temp, 'utf-8'))
-        search_word = re.sub(r'\<\/*\w+\>', '', self.entry.latin)
+        #search_word = re.sub(r'\<\/*\w+\>\w*', '', self.entry.latin)
         self.entry.headword = self.entry.latin.replace('<b>', '').replace('</b>', '')
-        self.entry.search = re.sub(r'[^a-zA-Z]+', '', search_word)
+        #self.entry.search = re.sub(r'[^a-zA-Z]+', '', search_word)
+        self.entry.search = self.searchWord(self.entry.latin)
         self.entry.root = self.rootGen(self.entry.search)
-        self.entry.ipa = self.phoneticize(self.entry.root)
+        self.entry.ipa = self.phoneticize(self.entry.search)
         self.entry.tags = self.makeTags(self.entry.notes, self.entry.part_of_speech, self.entry.headword)
         if self.entry.word_type == "postbase":
             self.entry.part_of_speech = "postbase"
@@ -37,11 +40,11 @@ class JsonEntry:
         
         return f"""
 {{"UUID":"{self.UUID.hexdigest()}",
-"search_word":"{self.entry.search}",
+"search_word":{self.entry.search},
 "headword":"{self.entry.headword}",
-"root":"{self.entry.root}",
+"root":{self.entry.root},
 "cyrillic":"{self.entry.cyrillic}",
-"ipa":"{self.entry.ipa}",
+"ipa":{self.entry.ipa},
 "jacobson":"{self.entry.coded_cyrillic}",
 "source_pos":"{self.entry.part_of_speech}",
 "pos":"<span class='tag {self.entry.pos}Tag'>{self.entry.pos.upper()}</span>",
@@ -81,43 +84,49 @@ class JsonEntry:
        #result = result.replace("</citation>", "</span>")
         return result
 
-    def phoneticize(self, word):
-        result = tokenize(word)
-        result = redouble(result)
-        result = convert2ipa(result)
-        result = tokens2string(result)
+    def phoneticize(self, roots):
+        result = []
+        for word in roots:
+            temp = tokenize(word)
+            temp = redouble(temp)
+            temp = convert2ipa(temp)
+            temp = tokens2string(temp)
+            result.append(temp)
         return result
 
-    def rootGen(self, word):
-        #verbs
-        if "-" in self.entry.latin:
-            result = word
-        #nouns: "kw"
-        elif word[-2:] == "kw":
-            result = word[:-2] + "w"
-        #nouns: "qw"
-        elif word[-2:] == "qw":
-            result = word[:-2] + "ghw"
-        #nouns: marked strong gh
-        elif "*" in self.entry.latin:
-            result = word[:-1] + "gh*"
-        #nouns: all other gh - "qikmiq"
-        elif word[-1] == "q":
-            result = word[:-1] + "gh"
-        #nouns: g - "sikik"
-        elif word[-1] == "k":
-            result = word[:-1] + "g"
-        #noun:e-underlying - "aaggaatae" 
-        elif word[-2:] == "ae":
-            result = word[:-2] + "e"
-        #vowel final roots: "repa"(N), "aafte"(V)
-        elif word[-1] in ["a", "i", "u", "e"]:
-            result = word
-        #nouns: te
-        elif word[-1] == "n":
-            result = word[:-1] + "te"
-        else:
-            result = word
+    def rootGen(self, roots):
+        result = []
+        for word in roots:
+            #verbs
+            if "-" in self.entry.latin:
+                temp = word
+            #nouns: "kw"
+            elif word[-2:] == "kw":
+                temp = word[:-2] + "w"
+            #nouns: "qw"
+            elif word[-2:] == "qw":
+                temp = word[:-2] + "ghw"
+            #nouns: marked strong gh
+            elif "*" in self.entry.latin:
+                temp = word[:-1] + "gh*"
+            #nouns: all other gh - "qikmiq"
+            elif word[-1] == "q":
+                temp = word[:-1] + "gh"
+            #nouns: g - "sikik"
+            elif word[-1] == "k":
+                temp = word[:-1] + "g"
+            #noun:e-underlying - "aaggaatae" 
+            elif word[-2:] == "ae":
+                temp = word[:-2] + "e"
+            #vowel final roots: "repa"(N), "aafte"(V)
+            elif word[-1] in ["a", "i", "u", "e"]:
+                temp = word
+            #nouns: te
+            elif word[-1] == "n":
+                temp = word[:-1] + "te"
+            else:
+                temp = word
+            result.append(temp)
         return result
 
     def simplifyPos(self, pos):
@@ -147,7 +156,7 @@ class JsonEntry:
             if freqCount[headword] < 10:
                 tagList+= uncommon 
         if "Chukotkan" in " ".join([f'"{gloss}"' for gloss in notes]):
-            tagList += chukotkan
+            tagList+= chukotkan
         if "emotional" in pos:
             tagList+= emoRoot
         if "postural" in pos:
@@ -164,6 +173,21 @@ class JsonEntry:
             tagList+= advPart
         
         return tagList
+    
+    def searchWord(self, latin):
+        result = []
+        search_word = []
+        if("," in latin):
+            search_word = self.entry.latin.split(", ")
+            for word in search_word:
+                word = re.sub(r'\<\/*\w+\>[e\d]*', '', word)
+                result.append(word)
+        else:
+            search_word = self.entry.latin
+            search_word = re.sub(r'\<\/*\w+\>[e\d]*', '', search_word)
+            search_word = re.sub(r'[^a-zA-Z]+', '', search_word)
+            result.append(search_word)
+        return result
 
 if __name__ == "__main__":
 
