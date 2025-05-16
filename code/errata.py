809dca2bea021d8d5098b88cfe0d088172684c82
BHunt <benjaminhunt93@gmail.com>
2025-05-16 12:41:26 -0500

    initial commit

---
+++ code/errata.py
@@ -0,0 +1,82 @@
+err = {
+    "bc018a46a6f016bf8a2462980b9d471c30111a93 ":{
+        #fix typo in second gloss somnething > something
+        #5/31/2023
+        #BH
+        "gloss":["to pack", "to store away something"],
+    },
+    "5e73e690fa0fff398123727775bec086749cb257":{
+        #headword missing supersript 'e'
+        #6/8/2023
+        #BH
+        "headword":"na<sup>e</sup>",
+    },
+    "39884c6387bb701bcb82870d54a4c95731235180 ":{
+        #ending of example mistakenly ended up in notes
+        #removed fragment from notes and added to end of example sentence
+        #7/30/2023
+        #BH
+        "notes":[],
+        "examples":["<span class='yupik_ex'>Iqallugestun meghmi kiyaghtaqut. Iwernga <i>aghveghet</i> iqallugunghitut.</span><span class='english_ex'>‘Like fish they live in the water however <i>whales</i> are not fish.’ (AANGHHAQ 64)</span>"],
+    },
+    "24439a6532ad3a9f6d52be79e43d9c49348bd091":{
+        #multiple entry headword; removed "nalla"
+        #4/15/2025
+        #BH
+        "headword":"nalla<sup>e</sup>",
+    },
+    "05f63d62a2dd8ec35e45c0475e6f37edaf12ee25":{
+        #remove example "kaziftaa ‘he spun it’"
+        #This example belongs to entry 828c2fb1a03c7429be971476bb9df2373e325265 - "kazifte-"
+        #4/15/2025
+        #BH
+        "examples":["<span class='yupik_ex'>kazivuq</span><span class='english_ex'>‘it spun around’</span>", "<span class='yupik_ex'>kazivutaa</span><span class='english_ex'>‘he spun around with it’</span>"],
+    },
+    "9dc37c79b648e0b76011fb5bbd8164d70bfa0ed3":{
+        #ataneq refers specifically to God on SLI
+        #according to Beulah and Tasi
+        #5/14/25
+        #BH
+    },
+    "0e963f7b2398eb0da54e27cdd508c42c44cad0d8":{
+        #aviin/aviita
+        #May not be a word according to Beulah and Tasi
+        #5/14/25
+        #BH
+    },
+    "ce32c8e75d1a5842d001e1d78d303c2c53b4bb3a":{
+        #conciousness and awareness meanings may be wrong on SLI
+        #according to Beulah and Tasi
+        #5/14/25
+        #BH
+    }
+}
+#natmun has incorrect parse > "natmun + natmun"
+#kinangaawa has wrong parse > "kinangaawa + [Abs][Unpd][Sg]" OR it is not an entry and should be further deconstructed
+#lots of issues with particles and demonstratives being labelled as nouns (some misspellings in the original data cause mislabelling)
+##naagu, nani
+
+additions = [
+    {
+        "UUID":"000001",
+        "search_word":["ughfik"],
+        "headword":"ughfik",
+        "root":["ughfig"],
+        "cyrillic":"уҳфик",
+        "ipa":["uχfik"],
+        "jacobson":"",
+        "source_pos":"noun",
+        "pos":"<span class='tag nounTag'>NOUN</span>",
+        "tags":"",
+        "gloss":["bathroom", "pants zipper"],
+        "notes":[],
+        "examples":[],
+        "source":"", #Tasi and Beulah (How should they be cited? Or maybe cite the language group and Kaalguq)
+        "etymology":"< ughhe- + -vik",
+        "semantic_code":"SLI",
+        "postbase_head_form":"",                     
+        "postbase_alphabetization_form":"",
+        "alphaA":"ughfik",
+        "alphaB":"ughfik"
+    },
+]
